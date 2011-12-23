d3f1dcfeeefc8dff51af935002e8dfb6afeb7edc
Rohan Kanade <github@rohankanade.com>
2011-12-23 11:10:13 +0530

    rough alerts system complete

--- webapi/Util.py
+++ webapi/Util.py
@@ -69,22 +69,124 @@ def checkAlerts(account,ip):
     #iterate of alerts, check process alert, disk usage alert, memory alerts. load average alert. [ process alert only check exists, all other check greater than]
     # if alert found, put it in openalerts for the account,
 def getLatestSnapShot(account,ip):
-    db = getdb(account)
-    dataColl = db[keys.DATA_PREFIX+ip.replace(".","")]
-    return dataColl.find().sort({keys.TIMESTAMP:pymongo.DESCENDING}).limit(1)
+    return getLastXSnapShot(account,ip,1)
 def checkDiskAlert(alert):
     hddName = alert[keys.ALERT_MAIN_OPERAND]
     ip = alert[keys.ALERT_FOR_IP]
     account = alert[keys.ALERT_FOR_ACCOUNT]
+    option = alert[keys.ALERT_MAJOR_OPTION]
+    operator = alert[keys.ALERT_OPERATOR]
+    rightoperand = alert[keys.ALERT_SEC_OPERAND]
     latestSnapShot = getLatestSnapShot(account,ip)
     for hdd in latestSnapShot[keys.SERVER_DISK_USAGE]:
-        return
+        if hddName == hdd[0]:
+            if option == keys.USED:
+                if operator == keys.OPERATOR_GREATER:
+                    if hdd[2] > rightoperand:
+                        return True,"Alert, Disk Space used for partition %s is greater than %s" %(hddName,rightoperand)
+                if operator == keys.OPERATOR_EQUAL:
+                    if hdd[2] == rightoperand:
+                        return True,"Alert, Disk Space used for partition %s is equal to %s" %(hddName,rightoperand)
+                if operator == keys.OPERATOR_LESS:
+                    if hdd[2] < rightoperand:
+                        return True,"Alert, Disk Space used for partition %s is less than %s" %(hddName,rightoperand)
+            if option == keys.FREE:
+                if operator == keys.OPERATOR_GREATER:
+                    if hdd[3] > rightoperand:
+                        return True,"Alert, Disk Space free for partition %s is greater than %s" %(hddName,rightoperand)
+                if operator == keys.OPERATOR_EQUAL:
+                    if hdd[3] == rightoperand:
+                        return True,"Alert, Disk Space free for partition %s is equal to %s" %(hddName,rightoperand)
+                if operator == keys.OPERATOR_LESS:
+                    if hdd[3] < rightoperand:
+                        return True,"Alert, Disk Space free for partition %s is less than %s" %(hddName,rightoperand)
+            if option == keys.PERCENTAGE_USED:
+                phdd = int(hdd[4].replace("%",""))
+                if operator == keys.OPERATOR_GREATER:
+
+                    if phdd > rightoperand:
+                        return True,"Alert, Disk Space used(%%) for partition %s is greater than %s" %(hddName,rightoperand)
+                if operator == keys.OPERATOR_EQUAL:
+                    if phdd == rightoperand:
+                        return True,"Alert, Disk Space used(%%) for partition %s is equal to %s" %(hddName,rightoperand)
+                if operator == keys.OPERATOR_LESS:
+                    if phdd < rightoperand:
+                        return True,"Alert, Disk Space used(%%) for partition %s is less than %s" %(hddName,rightoperand)
+    return False,""
 def checkProcessAlert(alert):
-    return
+    ProcessName = alert[keys.ALERT_MAIN_OPERAND]
+    ip = alert[keys.ALERT_FOR_IP]
+    account = alert[keys.ALERT_FOR_ACCOUNT]
+   # option = alert[keys.ALERT_MAJOR_OPTION]
+    operator = alert[keys.ALERT_OPERATOR]
+    #rightoperand = alert[keys.ALERT_SEC_OPERAND]
+    latestSnapShot = getLatestSnapShot(account,ip)
+    plist = []
+    for process in latestSnapShot[keys.PROCESSES]:
+        plist.append(process[10])
+    if operator == keys.OPERATOR_EXISTS:
+        if ProcessName not in plist:
+            return True,"Alert, Process '%s' does not exist" %ProcessName
+    if operator == keys.OPERATOR_PARTIAL_MATCH:
+        atleastone = False
+        for pname in plist:
+            if ProcessName in pname:
+                atleastone = True
+        if not atleastone:
+            return True,"Alert Process which contain %s does not exist" %ProcessName
+
+    return False,""
 def checkRamAlert(alert,ip):
-    return
+    ip = alert[keys.ALERT_FOR_IP]
+    account = alert[keys.ALERT_FOR_ACCOUNT]
+    option = alert[keys.ALERT_MAJOR_OPTION]
+    operator = alert[keys.ALERT_OPERATOR]
+    rightoperand = alert[keys.ALERT_SEC_OPERAND]
+    latestSnapShot = getLatestSnapShot(account,ip)
+    used = latestSnapShot[keys.MEM_USED]
+    free = latestSnapShot[keys.MEM_FREE]
+    if option == keys.USED:
+        if operator == keys.OPERATOR_GREATER:
+            if used > rightoperand:
+                return True,"Alert, Physical Memory used is greater than %s" %rightoperand
+        if operator == keys.OPERATOR_EQUAL:
+            if used == rightoperand:
+                return True,"Alert, Physical Memory used is equal to %s" %rightoperand
+        if operator == keys.OPERATOR_LESS:
+            if used < rightoperand:
+                return True,"Alert, Physical Memory used is less than %s" %rightoperand
+    if option == keys.FREE:
+        if operator == keys.OPERATOR_GREATER:
+            if free > rightoperand:
+                return True,"Alert, Physical Memory free is greater than %s" %rightoperand
+        if operator == keys.OPERATOR_EQUAL:
+            if free == rightoperand:
+                return True,"Alert, Physical Memory free is equal to %s" %rightoperand
+        if operator == keys.OPERATOR_LESS:
+            if free < rightoperand:
+                return True,"Alert, Physical Memory free is less than %s" %rightoperand
+    return False,""
 def checkLoadAvgAlert(alert):
-    return
+    ip = alert[keys.ALERT_FOR_IP]
+    account = alert[keys.ALERT_FOR_ACCOUNT]
+    operator = alert[keys.ALERT_OPERATOR]
+    option = alert[keys.ALERT_MAJOR_OPTION]
+    rightoperand = alert[keys.ALERT_SEC_OPERAND]
+    lastfewSnapShots = getLastXSnapShots(account,ip,option)
+    loadlist = []
+    for snapshot in lastfewSnapShots:
+        loadlist.append(float(snapshot[keys.LOADAVG]))
+    loadavg = mean(loadlist)
+    if operator == keys.OPERATOR_GREATER:
+        if loadavg > rightoperand:
+            return True,"Alert, LoadAverage is greater than %s" %rightoperand
+    if operator == keys.OPERATOR_EQUAL:
+        if loadavg == rightoperand:
+            return True,"Alert, LoadAverage is equal to %s" %rightoperand
+    if operator == keys.OPERATOR_LESS:
+        if loadavg < rightoperand:
+            return True,"Alert, LoadAverage is less than %s" %rightoperand
+    return False,""
 def getAlertsForIp(account):
     db = getdb(account)
     alertscoll = db[keys.ACC_SAVED_ALERTS+ip.replace(".","")]
@@ -95,7 +197,17 @@ def getAlertsForIp(account):
         return 0
     return alerts
 
+def mean(numberList):
+    if len(numberList) == 0:
+        return float('nan')
 
+    floatNums = [float(x) for x in numberList]
+    return sum(floatNums) / len(numberList)
+
+def getLastXSnapShot(account,ip,count):
+    db = getdb(account)
+    dataColl = db[keys.DATA_PREFIX+ip.replace(".","")]
+    return dataColl.find().sort({keys.TIMESTAMP:pymongo.DESCENDING}).limit(count)
 
 
 def getdb(account):
